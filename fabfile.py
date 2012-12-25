6edec5bd2fd6f99d39a991dc9246d7fc7b7b9d02
tomcz <github@tomczarniecki.com>
2012-12-24 23:54:14 -0500

    make sure to check that the SSH server is active on the host

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from fabric.api import *
-import aws, os, glob
+import aws, os, glob, time
 
 @task
 def check_credentials():
@@ -19,6 +19,8 @@ def make_vpc(vpc_name='midkemia'):
     check_credentials()
     bastion_hosts = aws.make_vpc(vpc_name)
     for host in bastion_hosts:
+        wait_for_ssh_connection(host)
+    for host in bastion_hosts:
         connect_script(host)
 
 @task
@@ -31,15 +33,25 @@ def delete_vpc(vpc_name='midkemia'):
     for path in glob.glob('connect_*'):
         os.remove(path)
 
-def connect_script(node):
-    filename = 'connect_' + node.name
+def connect_script(host):
+    filename = 'connect_' + host.name
     command = "ssh -i %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no %s@%s\n"
-    command = command % (node.keyfile, node.user, node.public_ip)
+    command = command % (host.keyfile, host.user, host.public_ip)
 
     with open(filename, 'w') as script:
         script.write('#!/bin/sh\n')
         script.write(command)
 
     os.chmod(filename, 0755)
-    print "Connect to [%s] instance using ./%s" % (node.name, filename)
+    print "Connect to [%s] instance using ./%s" % (host.name, filename)
 
+def wait_for_ssh_connection(host):
+    with settings(warn_only=True):
+        result = check_ssh(host)
+        while result.failed:
+            print 'Waiting for SSH service ...'
+            time.sleep(5)
+            result = check_ssh(host)
+
+def check_ssh(host):
+    return local('nc -z -v -w 10 %s 22' % host.public_ip)
